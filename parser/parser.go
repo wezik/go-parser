c13f4bc0e56092b6e3c5f620498052345b455e96
Dominik <wezik@users.noreply.github.com>
2024-04-23 17:51:40 +0200

    Parser progress initla progress reporting

--- parser/parser.go
+++ parser/parser.go
@@ -1,21 +1,47 @@
 package parser
 
 import (
+	"com/parser/ui"
+	"com/parser/ui/components"
 	"fmt"
 	"io"
+	"os"
 	"strings"
+	"sync"
 	"time"
 )
 
+type ProgressWrapper struct {
+	bytes int64
+}
 
+type ProgressCustomComponent struct {
+	bytesBar components.ProgressBar
+	label components.SimpleText
+}
 
-func ReadAndFlag(reader io.Reader) {
+func (pw ProgressCustomComponent) String() string {
+	return pw.label.String() + "\n" + pw.bytesBar.String() + "\n"
+}
+
+func ReadAndFlag(file *os.File) {
 	timestamp := time.Now().UnixMilli()
 	buffer := make([]byte, 1024)
 	logs := make([]string, 0)
 	builder := strings.Builder{}
+	fileStat, err := file.Stat()
+	if err != nil {
+		fmt.Println(err)
+	}
+	progressCh := make(chan ProgressWrapper)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		watchProgress(progressCh, fileStat.Size())
+	}()
 	for {
-		_, err := reader.Read(buffer)
+		bytes, err := file.Read(buffer)
 		if err == io.EOF {
 			break
 		} else if err != nil {
@@ -40,8 +66,47 @@ func ReadAndFlag(reader io.Reader) {
 		}
 		// Buffer can be filled with previous read, so we need to clear it
 		clear(buffer)
+		progressCh <- ProgressWrapper{bytes: int64(bytes)}
 	}
+	close(progressCh)
+	wg.Wait()
 	fmt.Println("Timestamps read:", len(logs))
 	fmt.Println("ReadAndFlag took", time.Now().UnixMilli()-timestamp, "ms")
 }
 
+func watchProgress(progressCh chan ProgressWrapper, fileSize int64) {
+	component := ProgressCustomComponent{
+		bytesBar: components.ProgressBarDefault(),
+		label: components.SimpleText{},
+	}
+	component.label.SetText("Reading file...")	
+	component.bytesBar.SetMax(fileSize)
+
+	ui.Render(component)
+
+	tickerStopCh := make(chan struct{})
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		ticker := time.NewTicker(200 * time.Millisecond)
+		defer ticker.Stop()
+		defer wg.Done()
+		for {
+			select {	
+			case <- ticker.C:
+				ui.ReRender(component)
+			case <- tickerStopCh:
+				return
+			}
+		}
+	}()
+
+	totalBytes := int64(0)
+	for progress := range progressCh {
+		totalBytes += progress.bytes
+		component.bytesBar.SetValue(totalBytes)
+	}
+	close(tickerStopCh)
+	wg.Wait()
+	ui.ReRender(component)
+}
